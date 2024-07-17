2e0b20f573cd3f3ac2c2f8fccf9f793b72fff757
Roddy <roddymcg787@gmail.com>
2024-07-17 01:03:00 -0700

    added theory of concept

---
+++ test.py
@@ -0,0 +1,8 @@
+# 3 pointer system
+"""
+can_exist = [a, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, k, q, j]
+                               ▲  ▲  ▲
+current_Card = 7
+# if current card= 7 does 8 or 6 exist in does exsit? if 8 or 6 exists, does 9 or 5 exist continue donw this path and choose best/
+doesexist = [x, x, x, x, x, x, x, x, x, x, x, x, x,]
+"""
